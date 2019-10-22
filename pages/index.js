24af9ca091f3927322dd8be5bce75605517579b6
Diego Ortiz <diegoortizpaez@gmail.com>
2019-10-22 09:29:26 -0500

    List of all the relevant information to calculate anual tax payment

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,109 @@ class Home extends React.Component {
         <h2 className={css.formTitle}>
           Deducciones
         </h2>
+        <ul>
+          <h1>Info personal</h1>
+          <li>Información reportada por terceros</li>
+          <li>Formulario del año anterior</li>
 
+          <h1>Productos financieros</h1>
+          <li>Certificado anual de retención en la fuente de cada banco</li>
+          <li>
+            <h3>Productos no bancarios</h3>
+            <ul>
+              <li>Comisionistas de bolsa</li>
+              <li>Acciones de sociedades privadas</li>
+              <li>Cooperativas de ahorro y compañias de financiamiento</li>
+              <li>Criptomonedas</li>
+              <li>Prástamos a terceros</li>
+              <li>Efectivo</li>
+            </ul>
+          </li>
+          <li>
+            <h3>Deudas no bancarias</h3>
+            <ul>
+              <li>Créditos educativos</li>
+              <li>Deudas con terceros</li>
+            </ul>
+          </li>
+
+          <h1>Bienes</h1>
+          <li>
+            <h3>Bienes Personales</h3>
+            <ul>
+              <li>Vehiculos personales</li>
+              <li>Bienes raices</li>
+              <li>Bienes raices sobre planos</li>
+              <li>Otros bienes o articulos personales</li>
+            </ul>
+          </li>
+          <li>
+            <h3>Bienes De Tu Negocio Personal</h3>
+            <ul>
+              <li>Maquinas o bienes de uso comercial</li>
+              <li>Inventarios</li>
+              <li>Bienes raices para desarrollo del negocio</li>
+            </ul>
+          </li>
+
+
+          <h1>Ingresos</h1>
+          <li>
+            <h3>Ingresos laborales o pensionales</h3>
+            <ul>
+              <li>Salario (Certificado ingresos y retenciones de cada trabajo)</li>
+              <li>Honorarios, servicios o comisiones como independiente (Cta cobro o cert+retenciones e Información de Gastos)</li>
+              <li>Negocio propio</li>
+              <li>Pensiones de jubilación</li>
+            </ul>
+          </li>
+          <li>
+            <h3>Ingresos por arrendamientos</h3>
+            <ul>
+              <li>Arrendamiento de bienes raices</li>
+              <li>Arrendamiento de vehículos</li>
+              <li>Arrendamiento de maquinaria</li>
+              <li>Arrendamiento de otros bienes</li>
+            </ul>
+          </li>
+          <li>
+            <h3>Ingresos por venta de bienes</h3>
+            <ul>
+              <li>Venta de vehículo</li>
+              <li>Venta de bienes raices incluyendo lote</li>
+              <li>Venta de acciones o participaciones en sociedades</li>
+              <li>
+                Venta de otros bienes
+                <ul>
+                  <li>(e.g. venta de computador a LEC y su rte/fte, con fecha aproximada)</li>
+                  <li>(e.g. compra del mismo)</li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <h3>Ingresos ocasionales</h3>
+            <ul>
+              <li>Herencia, legado o donación</li>
+              <li>Liquidación sociedad conyugal</li>
+              <li>Indemnisaciones y subsidios estatales</li>
+              <li>Seguros</li>
+              <li>Premios y loterias</li>
+              <li>Donaciones para partidos politicos</li>
+            </ul>
+          </li>
+
+          <h1>Aportes voluntarios a fondos de pensiones y retiro de cesantías</h1>
+          <li>Certificado de aportes y Retenciones de pensiones voluntarias Individual 2018 #1</li>
+          <li>Certificado tributario de cesantías año actual</li>
+          <li>Certificado tributario de cesantías año anterior</li>
+
+          <h1>Deducciones</h1>
+          <li>Medicina prepagada</li>
+          <li>Aportes como independiente a seguridad social</li>
+          <li>Dependientes</li>
+          <li>Donaciones</li>
+        </ul>
         <div className={css.cover}>
           <form className={css.formContainer}>
             <label htmlFor='de'>De</label>
