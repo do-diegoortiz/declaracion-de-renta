20aea817a4b8c11dc63aa16f701166f488778056
Diego Ortiz <diegoortizpaez@gmail.com>
2019-11-25 22:56:42 -0500

    Created _app.js and server.js  | Updated next.config to compile properly in dev

--- pages/_document.js
+++ pages/_document.js
@@ -13,14 +13,13 @@ class MyDocument extends Document {
     return (
       <Html lang="es">
         <Head>
-          <title>Declaracion de Renta</title>
           <link rel='icon' href='/favicon.ico' />
           <meta name="description" content="Optimiza tu declaración de renta para que no se te vaya un ojo de la cara en agosto del próximo año"></meta>
           <meta name="keywords" content="Declaración de renta, pensiones voluntarias, retención en la fuente"></meta>
-          <link rel="apple-touch-icon" href="/public/images/android-chrome-192x192.png"/>
-          <link rel="icon" sizes="192x192" href="/public/images/android-chrome-192x192.png"/>
-          <link rel="manifest" href="/public/manifest.json"/>
-          <link rel="shortcut icon" href="/public/images/favicon.ico"/>
+          <link rel="apple-touch-icon" href="/images/android-chrome-192x192.png"/>
+          <link rel="icon" sizes="192x192" href="/images/android-chrome-192x192.png"/>
+          <link rel="manifest" href="/manifest.json"/>
+          <link rel="shortcut icon" href="/images/favicon.ico"/>
         </Head>
         <body>
           <Main />
