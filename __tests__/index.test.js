3787fb324e9826e5810d3bac27e6846d53974119
Natalia Hashimoto <nahrivera7@gmail.com>
2020-01-16 10:44:29 -0500

    Setup CI (#14)
    * Update App tests
    
    * Update snapshot
    
    * Add circle ci config

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,27 +1,17 @@
 import { shallow } from 'enzyme'
 import React from 'react'
-import renderer from 'react-test-renderer'
+import ProviderMock from '../__mocks__/providerMock'
 
 import App from '../pages/index.js'
 
-describe('initial', () => {
-  test('first tests', () => {
-    expect(true).toBe(true)
-  })
-})
+describe('<App />', () => {
+  it('It render without crashing', () => {
+    const app = shallow(
+      <ProviderMock>
+        <App />
+      </ProviderMock>
+    )
 
-describe('With Enzyme', () => {
-  it('App shows right first parragraph', () => {
-    const app = shallow(<App />)
-
-    expect(app.find('p').first().text()).toEqual('Hay algunas cosas que puedes hacer para reducir legalmente el valor final a pagar por ingresos de trabajo del 2019. Pero las debes hacer antes del 31 de diciembre de 2019.')
-  })
-})
-
-describe('With Snapshot Testing', () => {
-  it('App shows "Hello world!"', () => {
-    const component = renderer.create(<App />)
-    const tree = component.toJSON()
-    expect(tree).toMatchSnapshot()
+    expect(app.length).toEqual(1)
   })
 })
