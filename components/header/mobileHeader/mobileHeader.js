b5f5aae98cbc77546684445da4b638cc7f3ef8a0
Natalia <nahrivera7@gmail.com>
2020-01-15 19:42:08 -0500

    Fix MobileHeader margin to show Introduction component correctly

--- components/header/mobileHeader/mobileHeader.js
+++ components/header/mobileHeader/mobileHeader.js
@@ -7,7 +7,7 @@ const MobileHeader = () => {
     <>
       <div className={css.bgFirstLayer}>
         <img className={css.logo} src={WhiteLogo} alt='Logo' />
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 30 500 80">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 30 500 50">
           <path fill="#FFF" fillOpacity="1" d="M0.00,49.98 C148.42,98.97 346.49,9.17 521.44,40.75 L500.00,150.00 L0.00,150.00 Z"></path>
         </svg>
       </div>
