c4bd85d24871bea66a2cddd4f71e4eb15035353c
Diego Ortiz <diegoortizpaez@gmail.com>
2020-02-08 20:37:32 -0500

    Fix Logo Quality

--- components/header/mobileHeader/mobileHeader.js
+++ components/header/mobileHeader/mobileHeader.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import WhiteLogo from '../../../public/images/whitelogo.svg';
 import css from './mobileHeader.scss'
 
 const MobileHeader = () => {
   return (
     <>
       <div className={css.bgFirstLayer}>
-        <img className={css.logo} src={WhiteLogo} alt='Logo' />
+        <img className={css.logo} src='/images/logo.png' alt='Logo' />
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 30 500 50">
           <path fill="#FFF" fillOpacity="1" d="M0.00,49.98 C148.42,98.97 346.49,9.17 521.44,40.75 L500.00,150.00 L0.00,150.00 Z"></path>
         </svg>
